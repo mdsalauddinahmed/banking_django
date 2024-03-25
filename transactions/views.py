3229d06b385b692a7609bad1c9ce82e80a9075ba
unknown <mdsalauddinahmed.amir@gmail.com>
2024-03-25 10:14:34 +0600

    first commit

---
+++ transactions/views.py
@@ -0,0 +1,28 @@
+from typing import Any
+from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+# Create your views here.
+
+from.models import Transaction
+class TransactionCreateMixin(LoginRequiredMixin,CreateView):
+    template_name=''
+    model=Transaction
+    title= ''
+    success_url = ''
+
+    def get_form_kwargs(self):
+        kwargs=super().get_form_kwargs()
+        kwargs.update({
+            'account':self.request.user.account,
+        })
+        return kwargs
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)  
+        context.update({
+            'title': self.title
+        })
+
+        return context
+      
+
